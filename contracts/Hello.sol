7b0d5239594dd391b01c471619745b51cba6a2b3
Mr-emeka <speak2c.emeka@gmail.com>
2022-03-09 08:49:43 +0100

    scafolding smart contract

---
+++ contracts/Hello.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.5.4;
+
+import "hardhat/console.sol";
+
+contract Hello {
+    uint public name;
+
+    constructor(){
+        console.log("Hello World");
+    }
+}
